db202798cf0dc16d98b5627066c4eba4fa40d28c
beat <adam_polczynski@yahoo.com>
2022-08-02 22:54:56 +0200

    removed unncesecary code

--- jest/env.ts
+++ jest/env.ts
@@ -1,3 +1,3 @@
 process.env.PORT = '8000'
-process.env.RAPID_API_KEY = '7ad94914fbmshbf1caebf406a988p1d7f26jsnbca0beb43c14'
+process.env.RAPID_API_KEY = '' //
 process.env.LOCATION = '/predictions/Cancun,Mexico'
